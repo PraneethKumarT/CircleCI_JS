3ad94b6960b017fe4030720df40898c6ce8b4471
PraneethKumarT <praneeth.kumt@gmail.com>
2024-04-18 11:47:25 -0400

    Initial commit

---
+++ src/tests/index.test.js
@@ -0,0 +1,11 @@
+test('basic arithmetic', () => {
+    expect(2 - 2).toBe(0);
+});
+
+test('basic arithmetic 2', () => {
+    expect(2 + 2).toBe(4);
+});
+
+test('basic arithmetic 2', () => {
+    expect(2 / 2).toBe(1);
+});
